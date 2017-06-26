5989bd7c98e77ded2fc6d0f3083594a8ff6ce232
heenamkung <bnamkung@gmail.com>
2017-06-27 01:34:17 +0900

    First commit

---
+++ sorting_music/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+#include <list>
+#include <vector>
+#include <Windows.h>
+#include <string>
+#include <utility>
+#include <direct.h>
+#include <fstream>
+
+
+int main() {
+	const int x = 1000;
+	char buff[x];
+	cout << _getcwd(buff, x);
+	cout << _mkdir("hello");
+	cout << rmdir("hello");
+	_chdir("C://Users/Hee/Documents");
+	cout << _getcwd(buff, x);
+	ifstream myfile;
+	myfile.open("rock.mp3");
+	char c;
+	while (myfile.get(c))
+		cout << c;
+
+	system("pause");
+}
